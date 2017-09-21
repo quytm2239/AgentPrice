263e24cfd07176ad8d7dc02b2d9b3df77ec49054
quytm2239 <quytm2239@gmail.com>
2017-09-21 21:12:33 +0700

    Update logic for TA n FPT shop

--- detect_content/index.js
+++ detect_content/index.js
@@ -1,49 +1,92 @@
 const shop = require('./shop');
 const htmlToJson = require('html-to-json');
 module.exports = {
+    getAllShop: function (keyword,completion) {
+        Promise.all([
+            module.exports.getTA(keyword),
+            module.exports.getFPT(keyword)
+        ]).then(success => {
+            var arrayFinal = [];
+            // 1. process for TA shop
+            var namesTA = success[0].names;
+            if (namesTA && namesTA.length > 0) {
+                let arrayTemp = [];
+                for (let i = 0;i < namesTA.length; i++) {
+                    arrayTemp.push({
+                        name: namesTA[i],
+                        price: success[0].prices[i],
+                        image: success[0].images[i],
+                        detail: success[0].details[i],
+                        supplier: 'Công ty cổ phần Thế giới số Trần Anh'
+                    });
+                }
+                arrayFinal.push(arrayTemp);
+            } else {
+                arrayFinal.push([]);
+            }
+            // 2. process for FPT shop
+            var namesFPT = success[1].names;
+            if (namesFPT && namesFPT.length > 0) {
+                let arrayTemp = [];
+                for (let i = 0;i < namesFPT.length; i++) {
+                    var mixedPrice = success[1].prices[i].replace('\r\n', '');
+                    var price = mixedPrice.substring(0,mixedPrice.indexOf('₫') + 1);
+                    var old_price = mixedPrice.substring(mixedPrice.indexOf('₫') + 1,mixedPrice.length).trim();
+                    arrayTemp.push({
+                        name: namesFPT[i],
+                        price: price,
+                        old_price: old_price,
+                        image: success[1].images[i],
+                        detail: success[1].details[i],
+                        supplier: 'Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT'
+                    });
+                }
+                arrayFinal.push(arrayTemp);
+            } else {
+                arrayFinal.push([]);
+            }
+            completion(null,arrayFinal);
+        }).catch(err => {
+            console.log(err);
+            completion(err,[]);
+        });
+    },
     getTA: function (keyword) {
         let url = shop.TA;
-        var searchUrl = url.replace('@', keyword);
+        var searchUrl = url.replace('@', encodeURI(keyword));
         return htmlToJson.request(searchUrl, {
-            'results': ['.col-lg-2', function ($div) {
-                return $div.find('.img-product').text();
+            'names': ['.product-name', function ($div) {
+                return $div.text().replace('\r\n', '').trim();
+            }],
+            'prices': ['.option-price', function ($div) {
+                return $div.find('.price').text().replace('\r\n', '').trim();
+            }],
+            'images': ['.img-product', function ($img) {
+                return $img.attr('data-original');
+            }],
+            'details': ['.product3', function ($a) {
+                return $a.attr('href');
             }]
-            // 'results': ['.col-lg-2', function ($div) {
-            //     return {
-            //         image: $div.find('.img-product').attr('data-original'),
-            //         name: $div.find('.product-name').text().replace('\r\n', '').trim(),
-            //         prices: $div.find('.option-price').find('.price').text().replace('\r\n', '').trim(),
-            //         detail_url: $div.find('.product3').attr('href'),
-            //         supplier: 'Công ty cổ phần Thế giới số Trần Anh'
-            //     };
-            // }]
         }, function (err, result) {
-            if (err) {
-                return err;
-            } else {
-                return result;
-            }
         });
     },
     getFPT: function (keyword) {
         let url = shop.FPTSHOP;
-        var searchUrl = url.replace('@', keyword);
+        var searchUrl = url.replace('@', encodeURI(keyword));
         return htmlToJson.request(searchUrl, {
-            'results': ['.fs-lpitem', function ($div) {
-                return {
-                    image: 'https:' + $div.find('.fs-icimg').find('img').attr('src'),
-                    name: $div.find('.fs-icname').text().replace('\r\n', '').trim(),
-                    prices: $div.find('.fs-icpri').text().replace('\r\n', '').trim(),
-                    detail_url: 'https://fptshop.com.vn' + $div.find('a').attr('href'),
-                    supplier: 'Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT'
-                };
+            'names': ['.fs-icname', function ($div) {
+                return $div.text().replace('\r\n', '').trim();
+            }],
+            'prices': ['.fs-icpri', function ($p) {
+                return $p.text().replace('\r\n', '').trim();
+            }],
+            'images': ['.fs-icimg', function ($p) {
+                return 'https:' + $p.find('img').attr('src');
+            }],
+            'details': ['.fs-lpitem', function ($div) {
+                return 'https://fptshop.com.vn' + $div.find('a').attr('href');
             }]
         }, function (err, result) {
-            if (err) {
-                return err;
-            } else {
-                return result;
-            }
         });
     }
 };
--- index.js
+++ index.js
@@ -23,37 +23,19 @@ const detect_content = require('./detect_content');
 app.get('/compare',function (req,res) {
     var keyword = req.query.keyword;
     if (keyword && keyword.length > 0){
-
-        Promise.all([
-            detect_content.getTA(keyword),
-            detect_content.getFPT(keyword)
-        ]).then(success => {
-            res.status(200).send([
-            {
-                message: 'Tìm thấy '+success[0].results.length+ ' phù hợp với từ khoá: '+keyword,
-                results: success[0].results
-            },
-            {
-                message: 'Tìm thấy '+success[1].results.length+ ' phù hợp với từ khoá: '+keyword,
-                results: success[1].results
+        detect_content.getAllShop(keyword,function (err,result) {
+            if (!err) {
+                res.status(200).send({
+                    message: 'Tim thay du lieu',
+                    results: result
+                });
+                return;
             }
-            ]);
-        }).catch(err => {
-            console.log(err);
+            res.status(500).send({
+                message: err,
+                results: []
+            });
         });
-
-
-        // detect_content.getTA(keyword,
-        // function(success) {
-        //     console.log(success);
-        //     res.status(200).send({
-        //         message: 'Tìm thấy '+success.results.length+ ' phù hợp với từ khoá: '+keyword,
-        //         results: success.results
-        //     });
-        // },
-        // function(err) {
-        //     res.status(500).send(err);
-        // });
     } else {
         res.status(400).send({
             message: 'Tìm thấy 0 phù hợp với từ khoá: '+keyword,
@@ -61,6 +43,10 @@ app.get('/compare',function (req,res) {
         });
     }
 });
+
+function completion(err,result) {
+
+}
 
 server.listen(process.env.PORT || 9090, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
